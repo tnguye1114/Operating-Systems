f5c7efb99df9d264485fbf005db47cc6bec91e30
tnguye1114 <tnguye14@uafs.edu>
2025-09-27 22:41:23 -0500

    Update functions.c

---
+++ exam1/functions.c
@@ -0,0 +1,13 @@
+// Function declaration
+int add(int a, int b);
+
+// Function definition
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int sum = add(3, 4);
+    printf("Sum: %d\n", sum);
+    return 0;
+}
